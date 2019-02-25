ff9959c0568575e23116604617629fdd3c22b2b7
13Fire13nyu <belash.margo@gmail.com>
2019-02-25 11:32:39 +0000

    добавлены в revert_string librev, revert_string.c, main.c и в swap swap.c, main.c

--- lab2/src/revert_string/librev/main.c
+++ lab2/src/revert_string/librev/main.c
@@ -1,4 +1,4 @@
-main.c#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
--- lab2/src/revert_string/librev/revert_string.c
+++ lab2/src/revert_string/librev/revert_string.c
@@ -1,11 +1,11 @@
-#include "librev/revert_string.h"
+#include "revert_string.h"
 
 void RevertString(char *str)
 {
     char tmp;
     int i = 0;
     int size = 0;
-    for (size; str[size] != '\0./'; size++);
+    for (size; str[size] != '\0'; size++);
     size--;
     while (i <= size) {
         tmp = str[i];
