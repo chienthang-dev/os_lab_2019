a33dccf99beb2871668a9cbbe18d5d54cf8f036c
13Fire13nyu <belash.margo@gmail.com>
2019-05-21 13:32:19 +0000

    done

--- lab7/src/udpclient.c
+++ lab7/src/udpclient.c
@@ -8,71 +8,75 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <time.h>
+#include <unistd.h>
 #include <signal.h>
+
 #include <getopt.h>
 
-//#define SERV_PORT 20001
-//#define BUFSIZE 1024
+
 #define SADDR struct sockaddr
 #define SLEN sizeof(struct sockaddr_in)
 
-pid_t currentPID;
-
 int main(int argc, char **argv) {
     int BUFSIZE = -1;
-    int SERV_PORT = -1;
-    char * ADDR;
-    while (1) {
-    int current_optind = optind ? optind : 1;
-
-    static struct option options[] = {{"BUFSIZE", required_argument, 0, 0},
-                                        {"SERV_PORT", required_argument, 0, 0},
-                                        {"ADDR", required_argument, 0, 0},
-                                      {0, 0, 0, 0}};
-
-    int option_index = 0;
-    int c = getopt_long(argc, argv, "", options, &option_index);
-
-    if (c == -1)
-      break;
-
-    switch (c) {
-    case 0: {
-      switch (option_index) {
-        case 0:
-            BUFSIZE = atoi(optarg);
-            if(BUFSIZE < 1)
-    		  BUFSIZE = -1;
-            break;
-        case 1:
-            SERV_PORT = atoi(optarg);
-            if(SERV_PORT < 1)
-        	  SERV_PORT = -1;
-            break;
-        case 2:
-            ADDR = optarg;
-            break;
-      default:
-        printf("Index %d is out of options\n", option_index);
-      }
-    } break;
-
-    case '?':
-      printf("Arguments error\n");
-      break;
-    default:
-      fprintf(stderr, "getopt returned character code 0%o?\n", c);
-    }
-  }
-      if (BUFSIZE == -1 || SERV_PORT == -1) {
-        printf("Usage: %s --BUFSIZE 100 --SERV_PORT 10050 \n", argv[0]);
-        return 1;
-      }
+	  int SERV_PORT = -1;
+	
+	  char* ADDR = "1";
+	  while (true) {
+	    int current_optind = optind ? optind : 1;
+	
+	    static struct option options[] = {{"BUFSIZE", required_argument, 0, 0},
+	                                      {"SERV_PORT", required_argument, 0, 0},
+	                                      {"ADDR", required_argument, 0, 0},
+	                                      {0, 0, 0, 0}};
+	  int option_index = 0;
+	    int c = getopt_long(argc, argv, "",options, &option_index);
+	
+	    if (c == -1) break;
+	
+	    switch (c) {
+	      case 0:
+	        switch (option_index) {
+	          case 0:
+	            BUFSIZE = atoi(optarg);
+	            if (BUFSIZE <= 0) {
+	                printf("BUFSIZE must be a positive number\n");
+	                return 1;
+	              }
+	            break;
+	
+	            case 1:
+	            SERV_PORT = atoi(optarg);
+	            if (SERV_PORT <= 0) {
+	                printf("SERV_PORT must be a positive number\n");
+	                return 1;
+	              }
+	            break;
+	
+	            case 2:
+	            ADDR = optarg;
+	            break;
+	        }
+	
+	      case '?':
+	        break;
+	
+	      default:
+	        printf("getopt returned character code 0%o?\n", c);
+	    }
+	
+	  }
+	  if (BUFSIZE == -1 || SERV_PORT == -1 || ADDR == "1") {
+	    printf("Usage: %s --bufsize \"buffer_size\" --serv_port \"serv_port\"\n",
+	           argv[0]);
+	    return 1;
+	  }  
   int sockfd, n;
   char sendline[BUFSIZE], recvline[BUFSIZE + 1];
   struct sockaddr_in servaddr;
   struct sockaddr_in cliaddr;
-  clock_t start, stop;
 
   memset(&servaddr, 0, sizeof(servaddr));
   servaddr.sin_family = AF_INET;
@@ -88,38 +92,19 @@ int main(int argc, char **argv) {
   }
 
   write(1, "Enter string\n", 13);
-  int flag = 0;
+
   while ((n = read(0, sendline, BUFSIZE)) > 0) {
-      if (flag!=0)
-        write (1, "Enter string\n", 13);
-      flag=1;
     if (sendto(sockfd, sendline, n, 0, (SADDR *)&servaddr, SLEN) == -1) {
       perror("sendto problem");
       exit(1);
     }
-    pid_t child_pid = fork();
-    currentPID = child_pid;
-    if (child_pid ==0){
-        if (recvfrom(sockfd, recvline, BUFSIZE, 0, NULL, NULL) == -1) {
-              perror("recvfrom problem");
-              printf("Connection problem on udp\n");
-              //exit(1);
-        }
-    }
-    else{
-        start=clock();
-        sleep(5);
-        printf("after sleep\n");
-        stop=clock();
-        kill(child_pid, SIGKILL);
-        printf("after kill\n");
+
+    if (recvfrom(sockfd, recvline, BUFSIZE, 0, NULL, NULL) == -1) {
+      perror("recvfrom problem");
+      exit(1);
     }
 
-  }
-    
     printf("REPLY FROM SERVER= %s\n", recvline);
-    if (flag!=1)
-    printf("Enter string!\n");
- 
+  }
   close(sockfd);
 }
